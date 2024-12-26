faabfa000d55cb22c9ac091d292cf53c6a06315b
KoKhant02 <kokitt@Cyfot02>
2024-12-26 20:56:30 +0630

    erc1155.sol

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -8,15 +8,12 @@ contract MeowjestyNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
     
-    // Address where the NFTs are stored
     address public owner;
 
-    // The constructor will set the initial name and symbol of the NFT
     constructor() ERC721("MeowjestyNFT", "MJ") {
-        owner = msg.sender; // Set the deployer as the contract owner
+        owner = msg.sender;
     }
 
-    // Function to mint a new NFT
     function mintNFT(string memory tokenURI) public returns (uint256) {
         require(msg.sender != address(0), "Invalid address");
         require(msg.sender == owner, "Only the owner can mint NFTs");
@@ -24,14 +21,12 @@ contract MeowjestyNFT is ERC721URIStorage {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         
-        // Mint the NFT and set its URI
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, tokenURI);
         
         return tokenId;
     }
 
-    // Get the current token count (for your future NFTs)
     function getCurrentTokenId() public view returns (uint256) {
         return _tokenIdCounter.current();
     }
