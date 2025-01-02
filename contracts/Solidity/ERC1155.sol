f6828b469435f27291f60b5d00217ecbfaa70e2f
KoKhant02 <kokitt@C01-DAT0131.dat.com>
2025-01-02 12:26:42 +0630

    go contract fixed

--- contracts/Solidity/ERC1155.sol
+++ contracts/Solidity/ERC1155.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../../@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import "../../@openzeppelin/contracts/access/Ownable.sol";
 
 contract VariantERC1155 is ERC1155URIStorage, Ownable {
 
